3878da415f3d23bae01e4b0f23cc93b37464cf06
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2023-03-12 15:11:05 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Max_Min
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Task = { 5, 2, 7, 3, 9, 1, 8, 6, 4 };
+            int min = Task[0];
+            int max = Task[0];
+            double sum = 0;
+
+           
+            foreach (int num in Task)
+            {
+                if (num < min)
+                {
+                    min = num;
+                }
+                if (num > max)
+                {
+                    max = num;
+                }
+                sum += num;
+            }
+
+            double avg = sum / Task.Length;
+
+           
+            Console.WriteLine("Мінімум: " + min);
+            Console.WriteLine("Максимум: " + max);
+            Console.WriteLine("Середнє арифметичне: " + avg);
+            Console.WriteLine("Сума: " + sum);
+        }
+    }
+}
